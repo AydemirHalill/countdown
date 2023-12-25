16efdfa2837e3bf712a0c9f35c0cbf5184382abe
Halil Aydemir <0halilaydemir@gmail.com>
2023-12-25 20:10:38 +0300

    upgrade

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const minutes = document.getElementById('minutes')
 const seconds = document.getElementById('seconds')
 
 const currentYear = new Date().getFullYear()
-const newYearTime = new Date(`January 1 ${currentYear + 3} 00:00:00`)
+const newYearTime = new Date(`January 1 ${currentYear + 1} 00:00:00`)
+const yearDisplay = document.getElementById('yearDisplay')
 
+const time = document.createElement('span')
+time.classList.add('text-danger', 'lead')
+
+time.innerHTML = ` ${currentYear} - ${newYearTime.getFullYear()} <small><b><i class="bi bi-hourglass-split"></i></b></small>`
+yearDisplay.appendChild(time)
 
 function updateCountdown(){
     const currentTime = new Date()
@@ -29,5 +35,4 @@ function updateCountdown(){
     minutes.innerHTML = min
     seconds.innerHTML = s
 }
-
 setInterval(updateCountdown, 1000)
